d0b5af0a7cbf8b65f4c8b3df51a406c3cc424a8e
Ryan Bullock <rrb3942@gmail.com>
2010-11-22 14:11:01 -0500

    Updated cmd_server.pl example to use new syntax and var passing

--- examples/cmd_server.pl
+++ examples/cmd_server.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 #Author: Ryan Bullock
-#Version: 0.1
+#Version: 0.2
 #Description: This provided a very simple command server for the asterisk manager interface.
 
 use strict;
@@ -13,7 +13,7 @@ use AnyEvent::Socket;
 use Asterisk::AMI;
 
 #Port to listen on
-my $port = 5039;
+my $port = 5080;
 
 #Delimiters
 my $EOL = "\015\012";
@@ -31,9 +31,6 @@ $list .= 'list - Displays this list' . $EOL . $EOR;
 #Keep a list of clients
 my %clients;
 
-#Mappings of actionids to clients
-my %mappings;
-
 #Connect to asterisk
 my $astman = Asterisk::AMI->new(PeerAddr => '127.0.0.1',
 				Username => 'test',
@@ -41,14 +38,15 @@ my $astman = Asterisk::AMI->new(PeerAddr => '127.0.0.1',
 				Timeout => 3, #Default timeout for all operations, 3 seconds
 				Keepalive => 60, #Send a keepalive every minute
 				on_error => sub { print "Error occured on socket\r\n"; exit; },
-				on_timeout => sub { print "Connection to asterisk timed out\r\n"; exit; }
+				on_timeout => sub { print "Connection to asterisk timed out\r\n"; exit; },
+                                Blocking => 0
 			);
 
 die "Unable to connect to asterisk" unless ($astman);
 
 #Callback on mailbox command
 sub mailboxcb {
-	my ($asterisk, $action) = @_;
+	my ($asterisk, $action, $client) = @_;
 
 	my $id = $action->{'ActionID'};
 	
@@ -62,13 +60,12 @@ sub mailboxcb {
 		$mbstr = 'Invalid Mailbox, or command failed' . $EOL;
 	}
 
-	$mappings{$id}->push_write($mbstr . $EOR);
-	delete $mappings{$id} if (exists $clients{$mappings{$id}});
+	$client->push_write($mbstr . $EOR);
 }
 
 #Callback on channels command
 sub chancb {
-	my ($asterisk, $action) = @_;
+	my ($asterisk, $action, $client) = @_;
 
 	my $id = $action->{'ActionID'};
 	
@@ -82,13 +79,12 @@ sub chancb {
 		$chanstr = 'No channels active' . $EOL;
 	}
 
-	$mappings{$id}->push_write($chanstr . $EOR);
-	delete $mappings{$id} if (exists $clients{$mappings{$id}});
+	$client->push_write($chanstr . $EOR);
 }
 
 #Callback on hangup command
 sub hangupcb {
-	my ($asterisk, $action) = @_;
+	my ($asterisk, $action, $client) = @_;
 
 	my $id = $action->{'ActionID'};
 
@@ -100,13 +96,12 @@ sub hangupcb {
 		$str = 'Failed to hangup channel';
 	}	
 
-	$mappings{$id}->push_write($str . $EOL . $EOR) if (exists $clients{$mappings{$id}});
-	delete $mappings{$id};
+	$client->push_write($str . $EOL . $EOR);
 }
 
 #Remove a client if they d/c or error
 sub remove_client {
-	delete $clients{$_[0]};
+        delete $clients{$_[0]};
 	$_[0]->destroy;
 	return 1;
 }
@@ -120,18 +115,14 @@ sub client_input {
 
 	return unless ($cmd[0]);
 
-	my $id;
-
 	if ($cmd[0] eq 'mailbox') {
-		$id = $astman->send_action({	Action => 'MailboxCount',
-				  		Mailbox => $cmd[1] . '@default',
-						CALLBACK => \&mailboxcb });
+		$astman->send_action({	Action => 'MailboxCount',
+				  	Mailbox => $cmd[1] . '@default' }, \&mailboxcb, undef, $handle);
 	} elsif ($cmd[0] eq 'channels') {
-		$id = $astman->send_action({Action => 'Status', CALLBACK => \&chancb});
+		$astman->send_action({Action => 'Status'}, \&chancb, undef, $handle);
 	} elsif ($cmd[0] eq 'hangup') {
-		$id = $astman->send_action({	Action => 'Hangup',
-						Channel => $cmd[1],
-						CALLBACK => \&hangupcb } );
+		$astman->send_action({	Action => 'Hangup',
+					Channel => $cmd[1] }, \&hangupcb, undef, $handle);
 	} elsif ($cmd[0] eq 'list') {
 		$handle->push_write($list);
 	} elsif ($cmd[0] eq 'quit') {
@@ -141,8 +132,6 @@ sub client_input {
 		$handle->push_write('Invalid Command' . $EOL . $EOR);
 	}
 	
-	$mappings{$id} = scalar($handle) if (defined $id);
-
 	return 1;
 }
 
@@ -163,7 +152,7 @@ sub new_client {
 	$handle->push_write('Connected to command server.' . $EOL);
 	$handle->push_write('Enter \'list\' for a list of commands' . $EOL);
 
-	$clients{$handle} = $handle;
+        $clients{$handle} = $handle;
 }
 
 #Our server to accept connections
